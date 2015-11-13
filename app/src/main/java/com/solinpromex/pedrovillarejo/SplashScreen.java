519650ce9755aa36cd4848b4705af8a99fa891a1
MODESTO VASCO FORNAS <modestovascofornas@MODESTOs-Mini.lan>
2015-11-12 22:49:45 -0700

    002-primer push ampliado

---
+++ app/src/main/java/com/solinpromex/pedrovillarejo/SplashScreen.java
@@ -0,0 +1,52 @@
+package com.solinpromex.pedrovillarejo;
+
+/**
+ * Created by modestovascofornas on 11/11/15.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+import com.parse.PushService;
+
+/**
+ * Created by modestovascofornas on 10/23/15.
+ */
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+
+
+
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3500);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
